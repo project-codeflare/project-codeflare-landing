b259ec886efb3aebc9ec74561657b32fa49c212f
Vince Picone <vp@vincepic.one>
2022-07-29 19:39:28 -0500

    feat: make twocolumn more universal, add media

--- components/TwoColumn/TwoColumn.tsx
+++ components/TwoColumn/TwoColumn.tsx
@@ -1,53 +1,54 @@
-import { ReactNode } from "react";
-import { Innovate, Teammates, Path } from "@carbon/pictograms-react";
+import { ReactNode, ElementType, Children } from "react";
 
 import styles from "./TwoColumn.module.scss";
 import { Grid } from "components/Grid";
-import Image from "next/future/image";
+import Image, { StaticImageData } from "next/future/image";
 import screen from "media/screen.png";
 
-const ListItem = ({
+export const TwoColumnFeature = ({
   children,
-  icon,
+  icon: Icon,
 }: {
-  icon: ReactNode;
+  icon: ElementType;
   children: ReactNode;
 }) => {
   return (
     <li className={styles.listItem}>
-      <div className={styles.iconWrapper}>{icon}</div>
+      <div className={styles.iconWrapper}>
+        {<Icon className={styles.icon} />}
+      </div>
       <p>{children}</p>
     </li>
   );
 };
 
-const TwoColumn = () => {
+type twoColumnProps = {
+  heading: ReactNode;
+  imageSrc: string | StaticImageData;
+  imageAlt: string;
+  children: ReactNode;
+};
+
+export const TwoColumn = ({
+  heading,
+  imageSrc,
+  imageAlt,
+  children,
+}: twoColumnProps) => {
   return (
     <Grid className={styles.wrapper}>
       <div className={styles.left}>
-        <p className={styles.heading}>
-          Empower your AI and ML teams to innovate where it matters. We‘ll
-          handle the rest.
-        </p>
-        <ul className={styles.list}>
-          <ListItem icon={<Path className={styles.icon} />}>
-            Minimize the effort and skills needed to scale your AI and ML
-          </ListItem>
-          <ListItem icon={<Innovate className={styles.icon} />}>
-            Access the latest innovations and experience in running data science
-            platforms
-          </ListItem>
-          <ListItem icon={<Teammates className={styles.icon} />}>
-            Run your workloads anywhere.
-          </ListItem>
-        </ul>
+        <p className={styles.heading}>{heading}</p>
+        <ul className={styles.list}>{children}</ul>
       </div>
-      <Image
-        className={styles.image}
-        src={screen}
-        placeholder="blur"
-        alt="Screen shot of Ray dashboard"
-      />
+      <div className={styles.right}>
+        <Image
+          className={styles.image}
+          src={imageSrc}
+          placeholder="blur"
+          alt={imageAlt}
+        />
+      </div>
     </Grid>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 import Head from "next/head";
+import { Innovate, Teammates, Path } from "@carbon/pictograms-react";
+
 import Hero from "components/Hero";
 import Callout from "components/Callout";
 import Intro from "components/Intro";
 import Lightbox from "components/Lightbox";
-import TwoColumn from "components/TwoColumn";
+import {
+  TwoColumn,
+  TwoColumnFeature,
+  TwoColumnPictogram,
+} from "components/TwoColumn";
 
 import heroImage from "media/hero.webp";
 import videoPoster from "media/poster.jpg";
-import videoSrc from "media/logo.mp4";
+import videoSrc from "media/codeflare.mp4";
+
+import dashSrc from "media/dashboard.jpg";
+import terminalSrc from "media/terminal.jpg";
 
 const Home: NextPage = () => {
   const [lightboxOpen, setLightboxOpen] = useState(false);
@@ -32,17 +41,52 @@ const Home: NextPage = () => {
           <source src={videoSrc} type="video/mp4" />
         </video>
       </Lightbox>
-      <TwoColumn />
-      <Callout heading="Case study">
-        A major semi-conductor manufacterer applied the framework to analyze and
-        optimize approximately <strong>100,000 pipelines</strong> for training
-        machine learning models. CodeFlare cut the time it took to execute each
-        pipeline from 4 hours to 15 minutes.
-        <br />
-        <a href="research.ibm.com">Request a demo →</a>
-      </Callout>
+      <TwoColumn
+        imageSrc={dashSrc}
+        imageAlt="CodeFlare dashboard screenshot"
+        heading="Empower your AI and ML teams to innovate where it matters. We‘ll
+    handle the rest."
+      >
+        <TwoColumnFeature icon={Path}>
+          Minimize the effort and skills needed to scale your AI and ML
+        </TwoColumnFeature>
+        <TwoColumnFeature icon={Innovate}>
+          Access the latest innovations and experience in running data science
+          platforms
+        </TwoColumnFeature>
+        <TwoColumnFeature icon={Teammates}>
+          Run your workloads anywhere.
+        </TwoColumnFeature>
+      </TwoColumn>
+      <TwoColumn
+        imageSrc={terminalSrc}
+        imageAlt="CodeFlare terminal screenshot"
+        heading="Empower your AI and ML teams to innovate where it matters. We‘ll
+    handle the rest."
+      >
+        <TwoColumnFeature icon={Path}>
+          Minimize the effort and skills needed to scale your AI and ML
+        </TwoColumnFeature>
+        <TwoColumnFeature icon={Innovate}>
+          Access the latest innovations and experience in running data science
+          platforms
+        </TwoColumnFeature>
+        <TwoColumnFeature icon={Teammates}>
+          Run your workloads anywhere.
+        </TwoColumnFeature>
+      </TwoColumn>
     </div>
   );
 };
 
+{
+  /* <Callout heading="Case study">
+  A major semi-conductor manufacterer applied the framework to analyze and
+  optimize approximately <strong>100,000 pipelines</strong> for training
+  machine learning models. CodeFlare cut the time it took to execute each
+  pipeline from 4 hours to 15 minutes.
+  <br />
+  <a href="research.ibm.com">Request a demo →</a>
+</Callout> */
+}
 export default Home;
